66011e3d7d5827fc9e5a25744dae6d20a24ac9c4
Keerthi Bunga <keerthibunga04@gmail.com>
2026-09-17 22:12:01 +0530

    Enhance reusable API assertions

--- utils/ApiAssertions.js
+++ utils/ApiAssertions.js
@@ -1,6 +1,7 @@
 const { expect } = require('@playwright/test');
 
 class ApiAssertions {
+
     static async expectSuccessfulResponse(response) {
         expect(response.ok()).toBeTruthy();
         expect(response.status()).toBe(200);
@@ -16,6 +17,33 @@ class ApiAssertions {
 
     static expectNotEmpty(value) {
         expect(value.length).toBeGreaterThan(0);
+    }
+
+    static expectObject(value) {
+        expect(value).toBeTypeOf('object');
+        expect(value).not.toBeNull();
+    }
+
+    static expectString(value) {
+        expect(typeof value).toBe('string');
+    }
+
+    static expectNumber(value) {
+        expect(typeof value).toBe('number');
+    }
+
+    static expectPositiveNumber(value) {
+        expect(typeof value).toBe('number');
+        expect(value).toBeGreaterThan(0);
+    }
+
+    static expectArrayContainsObject(array, propertyName) {
+        expect(Array.isArray(array)).toBeTruthy();
+        expect(array.length).toBeGreaterThan(0);
+
+        for (const item of array) {
+            expect(item).toHaveProperty(propertyName);
+        }
     }
 }
 
